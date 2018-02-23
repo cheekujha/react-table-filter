d53d281a1b3e980ab54d9373057fba74bde24048
realturik <realturik@mail.ru>
2018-02-23 14:36:10 +0700

    restore filters data and all label constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,9 +1,13 @@
 export const BLANK_LABEL = "(blank)";
+export const SEARCH_PLACEHOLDER = "search";
+export const SELECT_ALL_LABEL = "Select All";
 export const ASC_VALUE = "asc";
 export const DSC_VALUE = "dsc"; 
 
 export default {
 	BLANK_LABEL,
+	SEARCH_PLACEHOLDER,
+	SELECT_ALL_LABEL,
 	ASC_VALUE,
 	DSC_VALUE
 }
